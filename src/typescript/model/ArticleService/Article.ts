a4a9f8b80d7b5ccfd6a9570274cd6b1724fd612d
jaimegsandoval <jaime.devs@gmail.com>
2021-05-03 22:13:04 -0700

    Removed notes & commented code

--- src/typescript/model/ArticleService/Article.ts
+++ src/typescript/model/ArticleService/Article.ts
@@ -11,20 +11,3 @@ export default class Article {
     this.published_date = published_date;
   }
 }
-
-// export const createNewArticle = (articleData: Article): Article => {
-//   return new Article(
-//     articleData.title,
-//     articleData.link,
-//     articleData.source,
-//     articleData.published_date
-//   );
-// };
-
-// export const createArticleDomElements = (article: Article) => {
-//   const title = document.createElement('h1');
-//   title.textContent = 'ARTICLE: ' + article.title;
-//   title.classList.add('nasaText');
-
-//   document.body.appendChild(title);
-// };
---
+++ src/typescript/view/index.ts
@@ -0,0 +1,16 @@
+// export const createNewArticle = (articleData: Article): Article => {
+//   return new Article(
+//     articleData.title,
+//     articleData.link,
+//     articleData.source,
+//     articleData.published_date
+//   );
+// };
+
+// export const createArticleDomElements = (article: Article) => {
+//   const title = document.createElement('h1');
+//   title.textContent = 'ARTICLE: ' + article.title;
+//   title.classList.add('nasaText');
+
+//   document.body.appendChild(title);
+// };
