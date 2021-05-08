e2d8f564a77ed364a2c4bf1818f85ef03d5ec156
jaimegsandoval <jaime.devs@gmail.com>
2021-05-08 13:29:05 -0700

    Created dom elements for articles url and descriptions

--- src/typescript/view/dom-creation.ts
+++ src/typescript/view/dom-creation.ts
@@ -6,10 +6,25 @@ export const createArticleDomElements = (articles: Article[]) => {
     'article-container'
   ) as HTMLDivElement;
 
-  articles.forEach((articles: Article) => {
-    const text = document.createElement('h1');
-    text.textContent = `ARTICLE: ${articles.title}`;
-    return container?.appendChild(text);
+  articles.forEach((article: Article) => {
+    const title: string = article.title;
+    const link: string = article.link;
+    const date: string = new Date(article.published_date).toDateString();
+
+    const div = document.createElement('div') as HTMLDivElement;
+    const h1 = document.createElement('h1') as HTMLHeadingElement;
+    h1.textContent = title;
+
+    const url = document.createElement('a') as HTMLAnchorElement;
+    url.innerText = 'Source';
+    url.setAttribute('href', link);
+
+    const dateStr = document.createElement('p');
+    dateStr.textContent = date;
+
+    div.append(h1, url, date);
+
+    return container?.appendChild(div);
   });
 };
 
