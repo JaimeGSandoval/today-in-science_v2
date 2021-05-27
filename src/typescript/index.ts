0e3dc774fd3b34169584f74b546b21bb311d0367
jaimegsandoval <jaime.devs@gmail.com>
2021-05-26 20:16:53 -0700

    Added a larger margin-top value for main container in mobile

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -15,32 +15,6 @@ export const controller: Controller = new Controller(
   view
 );
 
-controller.start();
-
-// const sidenavDisplay = () => {
-//   const sidenav = document.getElementById('mobile-sidenav-container');
-//   if (sidenav?.style.display === 'block') {
-//     return (sidenav.style.display = 'none');
-//   }
-
-//   sidenav!.style.display = 'block';
-// };
-
-// const hamburgerMenu = document.getElementById('hamburger-menu-icon');
-// hamburgerMenu!.addEventListener('click', sidenavDisplay);
-
-// const closeBtn = document.getElementById('closebtn');
-// closeBtn!.addEventListener('click', sidenavDisplay);
-
-// const mobileSideNave = document.getElementById('mobile-sidenav-container');
-// mobileSideNave!.addEventListener('click', (e) => {
-//   const target = e.target as HTMLElement;
-
-//   if (target!.id === 'mobile-sidenav-container') {
-//     document.getElementById(target.id)!.style.display = 'none';
-//   }
-// });
-
 const addListeners = (arg: any) => {
   const subjects = document.querySelectorAll(`[data-subject=${arg}]`);
 
@@ -60,3 +34,5 @@ const addListeners = (arg: any) => {
 Object.values(subjects).forEach((subject) => {
   addListeners(subject);
 });
+
+controller.start();
